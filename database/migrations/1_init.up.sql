1156beaedbc2e804f21316a1feda99adc92c1841
Serveresty <cs.go.12228@gmail.com>
2024-10-15 10:52:34 +0300

    fixed user_id issue

--- database/migrations/1_init.up.sql
+++ database/migrations/1_init.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS "users_data" (
-    "id" SERIAL PRIMARY KEY,
+    "user_id" SERIAL PRIMARY KEY,
     "email" VARCHAR(255) UNIQUE NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "roles" (
     "role_id" SERIAL PRIMARY KEY,
-    "role_name" VARCHAR(255) UNIQUE NOT NULL,
+    "role_name" VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "users_roles" (

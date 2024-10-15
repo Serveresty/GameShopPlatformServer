c28c10e984221739a2033895efe0659d44b44441
Serveresty <cs.go.12228@gmail.com>
2024-10-15 10:25:29 +0300

    login and register controllers

--- internal/controllers/shop.controller.go
+++ internal/controllers/shop.controller.go
@@ -2,6 +2,11 @@ package controllers
 
 import (
 	"GameShopPlatformServer/internal/service"
+	"GameShopPlatformServer/models"
+	cerr "GameShopPlatformServer/pkg/custom-errors"
+	"GameShopPlatformServer/pkg/jwts"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,9 +20,62 @@ func NewShopController(serv *service.ShopService) *ShopController {
 }
 
 func (sc *ShopController) SignIn(c *gin.Context) {
+	token := c.GetHeader("Authorization")
 
+	_, err := jwts.ParseToken(token)
+	if err == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": cerr.ErrAlreadyAuthorized.Error()})
+		return
+	}
+
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userId, err := sc.shopServ.GetShopRepository().GetAuthUserData(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": cerr.ErrUserNotFound.Error()})
+		return
+	}
+
+	roles, err := sc.shopServ.GetShopRepository().GetUsersRoles(userId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": cerr.ErrUserNotFound.Error()})
+		return
+	}
+
+	userIDstr := strconv.Itoa(userId)
+	tokenNew, err := jwts.CreateToken(userIDstr, roles)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": cerr.ErrClaims.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenNew})
 }
 
 func (sc *ShopController) SignUp(c *gin.Context) {
+	token := c.GetHeader("Authorization")
 
+	_, err := jwts.ParseToken(token)
+	if err == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": cerr.ErrAlreadyAuthorized.Error()})
+		return
+	}
+
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = sc.shopServ.GetShopRepository().CreateUser(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": cerr.ErrAlreadyRegistered.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "user created"})
 }
